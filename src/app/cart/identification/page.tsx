90c8eebae63588b8a57544e2ffe75b2bb442270d
Allyson Freitas <alisonfr83@gmail.com>
2025-08-07 17:48:59 -0300

    feat: add cart summary component

--- src/app/cart/identification/page.tsx
+++ src/app/cart/identification/page.tsx
@@ -1,5 +1,4 @@
 import { eq } from "drizzle-orm";
-import { TruckElectricIcon } from "lucide-react";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 
@@ -7,6 +6,7 @@ import { db } from "@/db";
 import { cartTable, shippingAddressTable } from "@/db/schema";
 import { auth } from "@/lib/auth";
 
+import CartSummary from "../components/cart-summary";
 import Addresses from "./components/addresses";
 
 const IdentificationPage = async () => {
@@ -47,6 +47,18 @@ const IdentificationPage = async () => {
       <Addresses
         shippingAddresses={shippingAddresses}
         defaultShippingAddressId={cart.shippingAddress?.id || null}
+      />
+      <CartSummary
+        subtotalInCents={cartTotalInCents}
+        totalInCents={cartTotalInCents}
+        products={cart.items.map((item) => ({
+          id: item.productVariant.id,
+          name: item.productVariant.product.name,
+          variantName: item.productVariant.name,
+          quantity: item.quantity,
+          priceInCents: item.productVariant.priceInCents,
+          imageUrl: item.productVariant.imageUrl,
+        }))}
       />
     </div>
   );
